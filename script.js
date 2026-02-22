c681d0e46dbe2ef0a526a62c95954631e5a84439
nhazz <42564555+nhazzazi@users.noreply.github.com>
2026-02-22 12:36:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 document.getElementById("year").textContent = new Date().getFullYear();
-
-// Email reveal (reduces basic scraping)
-const user = "nhazzazi";
-const domain = "ieee.org";
-const email = `${user}@${domain}`;
-
-const emailLink = document.getElementById("emailLink");
-emailLink.textContent = email;
-emailLink.href = `mailto:${email}`;
